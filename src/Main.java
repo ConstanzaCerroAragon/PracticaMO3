0248cfd68a933118197d7acbb3409d4dc2b8ff9e
Satenik <saten914@gmail.com>
2024-02-09 10:30:18 +0100

    Todo arreglado

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,20 @@ public class Main {
             public static String[][] clientes = new String[100][2];
             public static String[][] mecanicos = new String[100][3];
 
-            // Declaración del array de clientes
+            public static Scanner Scanner = new Scanner(System.in);
+            public static String[][] vehicles = new String[100][3]; // Array para almacenar los datos de los vehículos
+            public static String[][] reparaciones = new String[100][3]; // Array para almacenar los datos de las reparaciones
 
-    public static Scanner Scanner = new Scanner(System.in);
-    public static String[][] vehicles = new String[100][3]; // Array para almacenar los datos de los vehículos
-    public static String[][] reparaciones = new String[100][3]; // Array para almacenar los datos de las reparaciones
-    //Variables
-    // Encontrar el primer mecánico libre
-    String codiMecanic = "";
+            public static void main(String[] args) {
 
-
-    // Establecer el estado de la reparación
-    String estadoReparacion = "en curso";
-
-
-    public static void main(String[] args) {
+                Main main = new Main();
+                main.init();
+            }
+            String codiMecanic = "";
+            String estadoReparacion = "en curso";
 
 
-        Main main = new Main();
-        main.init();
-    }
-
-            public void init(){
+            public void init() {
 
                 int menuItem = 0;
                 do {
@@ -39,11 +31,11 @@ public class Main {
                     System.out.println("[5] Salir");
                     System.out.println("Selecciona una opcion: ");
 
-                    if (input.hasNextInt()){
+                    if (input.hasNextInt()) {
                         menuItem = input.nextInt();
                         input.nextLine();
                         String dni;
-                        switch (menuItem){
+                        switch (menuItem) {
 
                             case 1:
                                 //Llama al Metodo para pedir el dni y validarlo
@@ -55,7 +47,6 @@ public class Main {
                                         System.out.println("El formato del DNI no es válido. Debe ser de 8 dígitos seguidos de 1 letra.");
                                     }
                                 } while (!validarFormatoDNI(dni));
-
 
 
                                 // Pedir al usuario que ingrese el nombre del nuevo cliente
@@ -72,148 +63,150 @@ public class Main {
                                 altaMecanico(input, mecanicos);
                                 break;
 
-                    case 3:
-                        System.out.println("Introduce los datos del nuevo vehículo:");
-                        String matricula;
-                        String model;
-                        String dniPropietario;
-                        do {
-                            System.out.print("Introduce la matrícula del vehículo (formato: 4 dígitos seguidos de 3 letras): ");
-                            matricula = Scanner.nextLine();
+                            case 3:
+                                System.out.println("Introduce los datos del nuevo vehículo:");
+                                String matricula;
+                                String model;
+                                String dniPropietario;
+                                do {
+                                    System.out.print("Introduce la matrícula del vehículo (formato: 4 dígitos seguidos de 3 letras): ");
+                                    matricula = Scanner.nextLine();
 
-                            // Validar el formato de la matrícula utilizando una expresión regular
-                            if (!matricula.matches("\\d{4}[a-zA-Z]{3}")) {
-                                System.out.println("Error: El formato de la matrícula es incorrecto.");
-                            }
-                            System.out.print("Introduce el modelo del vehículo: ");
-                            model = Scanner.nextLine();
-                            System.out.print("Introduce el DNI del propietario del vehículo: ");
-                            dniPropietario = Scanner.nextLine();
+                                    // Validar el formato de la matrícula utilizando una expresión regular
+                                    if (!matricula.matches("\\d{4}[a-zA-Z]{3}")) {
+                                        System.out.println("Error: El formato de la matrícula es incorrecto.");
+                                    }
+                                    System.out.print("Introduce el modelo del vehículo: ");
+                                    model = Scanner.nextLine();
+                                    System.out.print("Introduce el DNI del propietario del vehículo: ");
+                                    dniPropietario = Scanner.nextLine();
 
-                            if (matricula.isEmpty() || model.isEmpty() || dniPropietario.isEmpty()) {
-                                System.out.println("Error: Todos los datos del vehículo son obligatorios.");
-                            }
-                        } while (matricula.isEmpty() || model.isEmpty() || dniPropietario.isEmpty());
+                                    if (matricula.isEmpty() || model.isEmpty() || dniPropietario.isEmpty()) {
+                                        System.out.println("Error: Todos los datos del vehículo son obligatorios.");
+                                    }
+                                } while (matricula.isEmpty() || model.isEmpty() || dniPropietario.isEmpty());
 
-                        // Comprobación de si la matrícula ya existe
-                        for (int i = 0; i < vehicles.length; i++) {
-                            if (vehicles[i][0] != null && vehicles[i][0].equals(matricula)) {
-                                System.out.println("Error: La matrícula ya existe.");
-                                return;
-                            }
-                        }
-
-                        // Comprobación de si existe el DNI del propietario
-                        boolean dniValido = false;
-                        for (int i = 0; i < clientes.length; i++) {
-                            if (clientes[i][0] != null && dniPropietario.matches(clientes[i][0])) {
-                                dniValido = true;
-                                break;
-                            }
-                        }
-                        if (!dniValido) {
-                            System.out.println("Error: DNI de propietario no válido.");
-                            return;
-                        }
-                        // Añadir el vehículo al array de vehículos
-                        for (int i = 0; i < vehicles.length; i++) {
-                            if (vehicles[i][0] == null) {
-                                vehicles[i][0] = matricula;
-                                vehicles[i][1] = model;
-                                vehicles[i][2] = dniPropietario;
-                                System.out.println("Vehículo dado de alta correctamente.");
-                                break;
-                            }
-                        }
-
-                    case 4:
-                        // Mostrar el listado de matrículas de vehículos dados de alta
-                        System.out.println("Listado de matrículas de vehículos dados de alta:");
-                        for (int i = 0; i < vehicles.length; i++) {
-
-                            if (vehicles[i][0] != null) {
-                                System.out.println(vehicles[i][0]);
-                            }
-                        }
-
-                        // Solicitar al usuario que seleccione una matrícula
-                        System.out.print("Introduce la matrícula del vehículo para la reparación: ");
-                        matricula = Scanner.nextLine();
-
-                        // Validar que la matrícula introducida exista en el listado de vehículos
-                        boolean matriculaExistente = false;
-                        for (int i = 0; i < vehicles.length; i++) {
-                            if (vehicles[i][0] != null && vehicles[i][0].equals(matricula)) {
-                                matriculaExistente = true;
-                                break;
-                            }
-                        }
-
-                        if (!matriculaExistente) {
-                            System.out.println("Error: La matrícula introducida no corresponde a ningún vehículo.");
-                            return;
-                        }
-
-                        // Array de reparaciones
-                        for (int i = 0; i < reparaciones.length; i++) {
-                            if (reparaciones[i][0] == null) {
-                                reparaciones[i][0] = matricula;
-                                reparaciones[i][1] = codiMecanic;
-                                reparaciones[i][2] = estadoReparacion;
-                                System.out.println("Reparación registrada correctamente.");
-                                break;
-                            }
-                        }
-                        break;
-                    case 5:
-                        boolean reparacionExistente=false;
-                        for (int i = 0; i < reparaciones.length; i++) {
-                            for (int j = 0; j < reparaciones[i].length; j++) {
-                                if (reparaciones[i][j] != null && !reparaciones[i][j].isEmpty()) {
-                                    matriculaExistente = true;
-                                    break;
+                                // Comprobación de si la matrícula ya existe
+                                for (int i = 0; i < vehicles.length; i++) {
+                                    if (vehicles[i][0] != null && vehicles[i][0].equals(matricula)) {
+                                        System.out.println("Error: La matrícula ya existe.");
+                                        return;
+                                    }
                                 }
-                            }
+
+                                // Comprobación de si existe el DNI del propietario
+                                boolean dniValido = false;
+                                for (int i = 0; i < clientes.length; i++) {
+                                    if (clientes[i][0] != null && dniPropietario.matches(clientes[i][0])) {
+                                        dniValido = true;
+                                        break;
+                                    }
+                                }
+                                if (!dniValido) {
+                                    System.out.println("Error: DNI de propietario no válido.");
+                                    return;
+                                }
+                                // Añadir el vehículo al array de vehículos
+                                for (int i = 0; i < vehicles.length; i++) {
+                                    if (vehicles[i][0] == null) {
+                                        vehicles[i][0] = matricula;
+                                        vehicles[i][1] = model;
+                                        vehicles[i][2] = dniPropietario;
+                                        System.out.println("Vehículo dado de alta correctamente.");
+                                        break;
+                                    }
+                                }
+
+                            case 4:
+                                // Mostrar el listado de matrículas de vehículos dados de alta
+                                System.out.println("Listado de matrículas de vehículos dados de alta:");
+                                for (int i = 0; i < vehicles.length; i++) {
+
+                                    if (vehicles[i][0] != null) {
+                                        System.out.println(vehicles[i][0]);
+                                    }
+                                }
+
+                                // Solicitar al usuario que seleccione una matrícula
+                                System.out.print("Introduce la matrícula del vehículo para la reparación: ");
+                                matricula = Scanner.nextLine();
+
+                                // Validar que la matrícula introducida exista en el listado de vehículos
+                                boolean matriculaExistente = false;
+                                for (int i = 0; i < vehicles.length; i++) {
+                                    if (vehicles[i][0] != null && vehicles[i][0].equals(matricula)) {
+                                        matriculaExistente = true;
+                                        break;
+                                    }
+                                }
+
+                                if (!matriculaExistente) {
+                                    System.out.println("Error: La matrícula introducida no corresponde a ningún vehículo.");
+                                    return;
+                                }
+
+                                // Array de reparaciones
+                                for (int i = 0; i < reparaciones.length; i++) {
+                                    if (reparaciones[i][0] == null) {
+                                        reparaciones[i][0] = matricula;
+                                        reparaciones[i][1] = codiMecanic;
+                                        reparaciones[i][2] = estadoReparacion;
+                                        System.out.println("Reparación registrada correctamente.");
+                                        break;
+                                    }
+                                }
+                                break;
+                            case 5:
+                                boolean reparacionExistente = false;
+                                for (int i = 0; i < reparaciones.length; i++) {
+                                    for (int j = 0; j < reparaciones[i].length; j++) {
+                                        if (reparaciones[i][j] != null && !reparaciones[i][j].isEmpty()) {
+                                            matriculaExistente = true;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (reparacionExistente = false) {
+                                    System.out.println("No hay vehículos con reparacion en curso");
+                                    return;
+                                } else {
+                                    for (int i = 0; i < reparaciones.length; i++) {
+                                        System.out.println(reparaciones[i][2]);
+                                    }
+                                    System.out.println();
+                                }
+                                System.out.println("Introduce el número de filas de la matrícula que quieres modificar el estado");
+                                int fila = Scanner.nextInt();
+
+                                if (fila < 0 || fila >= reparaciones.length) {
+                                    System.out.println("Coordenadas fuera de rango.");
+                                    return;
+                                }
+                                Scanner.nextLine();
+
+                                System.out.print("Introduce el nuevo valor: ");
+                                String nuevoValor = Scanner.nextLine();
+
+                                reparaciones[fila][2] = nuevoValor;
+
+                                System.out.println("Dato modificado correctamente.");
+
+                            case 6:
+                                System.out.println("Saliedo...");
+                                break;
+                            default:
+                                System.out.println("Opción no vàlida");
                         }
-                        if (reparacionExistente=false) {
-                            System.out.println("No hay vehículos con reparacion en curso");
-                            return;
-                        } else {
-                            for (int i = 0; i < reparaciones.length; i++) {
-                                System.out.println (reparaciones[i][2]);
-                            }
-                            System.out.println();
-                        }
-                        System.out.println("Introduce el número de filas de la matrícula que quieres modificar el estado");
-                        int fila = Scanner.nextInt();
-
-                        if (fila < 0 || fila >= reparaciones.length) {
-                            System.out.println("Coordenadas fuera de rango.");
-                            return;
-                        }
-                        Scanner.nextLine();
-
-                        System.out.print("Introduce el nuevo valor: ");
-                        String nuevoValor = Scanner.nextLine();
-
-                        reparaciones[fila][2] = nuevoValor;
-
-                        System.out.println("Dato modificado correctamente.");
-
-                    case 6:
-                        System.out.println("Saliedo...");
-                        break;
-                    default:
+                    } else {
                         System.out.println("Opción no vàlida");
-                }
-            }else{
-                System.out.println("Opción no vàlida");
+                    }
+                    Scanner.nextLine();
+                    System.out.println("");
+
+                } while (menuItem != 6);
             }
-            Scanner.nextLine();
-            System.out.println("");
 
-        }while(menuItem!=6);
-
+    //CASE 1: DAR DE ALTA NUEVO EMPLEADO
 
     /**
      * Validar el formato del DNI
@@ -225,8 +218,6 @@ public class Main {
     public static boolean validarFormatoDNI(String dni) {
         return dni.matches("[0-9]{8}[a-zA-Z]");
     }
-
-
 
     /**
      * Pide el DNI al usuario
@@ -354,6 +345,4 @@ public class Main {
 
 }
 
-
-}
 
